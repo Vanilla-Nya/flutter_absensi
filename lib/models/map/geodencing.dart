595cd86e82c19b4f3535d02bfe730b41343b50e7
Miharin <asunayuuki289@gmail.com>
2024-05-10 09:38:38 +0700

    make geofence

---
+++ lib/models/map/geodencing.dart
@@ -0,0 +1,44 @@
+import 'package:geolocator/geolocator.dart';
+
+class SquareGeoFencing {
+  const SquareGeoFencing({
+    required this.id,
+    required this.latitudeStart,
+    required this.latitudeEnd,
+    required this.longitudeStart,
+    required this.longitudeEnd,
+  });
+
+  final String id;
+  final double latitudeStart;
+  final double latitudeEnd;
+  final double longitudeStart;
+  final double longitudeEnd;
+}
+
+class GeoFencing {
+  const GeoFencing({this.listSquareGeoFencing});
+  const GeoFencing.square({required this.listSquareGeoFencing});
+  final List<SquareGeoFencing>? listSquareGeoFencing;
+
+  Future<bool> isInsideSquareGeoFencing() async {
+    Position position = await Geolocator.getCurrentPosition();
+    for (var geoFencing in listSquareGeoFencing!) {
+      if (position.latitude
+              .isBetween(geoFencing.latitudeStart, geoFencing.latitudeEnd) &&
+          position.longitude
+              .isBetween(geoFencing.longitudeStart, geoFencing.longitudeEnd)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
+  }
+}
+
+extension Range on double {
+  bool isBetween(double from, double to) {
+    return from <= this && this <= to;
+  }
+}
--- lib/screens/protected/signin_signout/signin_signout.dart
+++ lib/screens/protected/signin_signout/signin_signout.dart
@@ -1,24 +1,16 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_praktek_dokter/helpers/protected/signin_signout_helper.dart';
-import 'package:flutter_praktek_dokter/widget/custom_button/custom__text_button.dart';
+import 'package:flutter_praktek_dokter/models/map/geodencing.dart';
 import 'package:flutter_praktek_dokter/widget/custom_button/custom_filled_button.dart';
 import 'package:flutter_praktek_dokter/widget/custom_card/custom_card.dart';
 import 'package:flutter_praktek_dokter/widget/custom_choice_chip/custom_choice_chip.dart';
 import 'package:flutter_praktek_dokter/widget/custom_textfromfield/custom_textformfield.dart';
 import 'package:gap/gap.dart';
-import 'package:geofence_service/geofence_service.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 class SigninSignout extends StatelessWidget {
   SigninSignout({super.key});
   final SigninSignOutHelper _controller = Get.put(SigninSignOutHelper());
-  final double latitudeStart = -7.9791797;
-  final double latitudeEnd = -7.9793799;
-  final double longitudeStart = 113.9933635;
-  final double longitudeEnd = 113.9933571;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -32,16 +24,64 @@ class SigninSignout extends StatelessWidget {
                 child: CustomFilledButton(
                   label: "Check in",
                   onPressed: () async {
-                    Position position = await Geolocator.getCurrentPosition();
-                    if ((position.latitude >= latitudeStart &&
-                            position.latitude <= latitudeEnd) &&
-                        (position.longitude >= longitudeStart &&
-                            position.longitude <= longitudeEnd)) {
-                      print("true");
-                      print(position);
-                    } else {
-                      print("false");
-                      print(position);
+                    final bool isInside = await const GeoFencing.square(
+                        listSquareGeoFencing: <SquareGeoFencing>[
+                          SquareGeoFencing(
+                            id: "Puskesmas Sumber Wringin",
+                            latitudeStart: -7.9791797,
+                            latitudeEnd: -7.9793799,
+                            longitudeStart: 113.9933635,
+                            longitudeEnd: 113.9933571,
+                          ),
+                        ]).isInsideSquareGeoFencing();
+                    if (isInside) {
+                      if (!context.mounted) return;
+                      return showModalBottomSheet(
+                        constraints: BoxConstraints(
+                          minWidth: MediaQuery.of(context).size.width,
+                          minHeight: MediaQuery.of(context).size.height * 0.5,
+                          maxHeight: MediaQuery.of(context).size.height * 0.5,
+                        ),
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Stack(
+                            alignment: Alignment.center,
+                            fit: StackFit.expand,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    CustomChoiceChip(
+                                      title: 'Alasan',
+                                      content: const ["Sakit", "Ijin"],
+                                      length: 2,
+                                    ),
+                                    const Gap(10.0),
+                                    Obx(() => _controller.value.value == 1
+                                        ? CustomTextFormField(
+                                            label: "Alasan",
+                                            verification: true,
+                                            maxlines: 3,
+                                            keyboardType:
+                                                TextInputType.multiline,
+                                          )
+                                        : const Flexible(child: Text(""))),
+                                  ],
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: CustomFilledButton(
+                                  onPressed: () {},
+                                  label: "Submit",
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     }
                   },
                 ),
