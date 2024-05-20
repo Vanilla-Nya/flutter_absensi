2cc37ae5c38d4e2d22fe5402499a491e858b20f5
Vanilla-Nya <fontaro990@gmail.com>
2024-05-20 19:17:50 +0700

    .

--- lib/helpers/protected/table_user_helper.dart
+++ lib/helpers/protected/table_user_helper.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_absensi/helpers/global/globals.dart';
 import 'package:get/get.dart';
 
 class TableUserHelper extends GetxController {
@@ -19,6 +22,7 @@ class TableUserHelper extends GetxController {
   handleSubmitAddDataContent() {
     tableContent.add({...addNewTableContentData});
     tableContent.refresh();
+    handleAddToDatabase();
     for (var element in controller) {
       element.clear();
     }
@@ -27,5 +31,43 @@ class TableUserHelper extends GetxController {
   handleNewTableContentOnSubmit() {
     tableContent.add(addNewTableContentData);
     tableContent.refresh();
+  }
+
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  handleAddToDatabase() async {
+    try {
+      await auth
+          .createUserWithEmailAndPassword(
+              email: addNewTableContentData["Email"],
+              password: addNewTableContentData["Password"])
+          .then((userCredential) async {
+        if (userCredential.user?.uid.runtimeType != null) {
+          final users = db.collection("Users");
+          await users.doc(userCredential.user?.uid).set({
+            "email": addNewTableContentData["Email"],
+            "name": addNewTableContentData["Nama"],
+            "password": addNewTableContentData["Pawssword"],
+            "role": addNewTableContentData["Role"],
+            "telp_number": addNewTableContentData["No.Telp"],
+          }).then((result) => Get.snackbar("Penambahan User Berhasil",
+              "User Dengan Nama ${addNewTableContentData["Nama"]} Berhasil Di Tambahkan"));
+        }
+      });
+    } on FirebaseAuthException catch (error) {
+      String errorMassage = "";
+      if (error.code == "netwrok=request-failed") {
+        errorMassage = "Database Timeout";
+      } else if (error.code == "invalid-credential") {
+        errorMassage = "Email Atau Password Salah";
+      }
+      Get.snackbar(
+        "Penambahan User Gagal",
+        errorMassage,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    } catch (error) {
+      debugPrint(error.toString());
+    }
   }
 }
